90244357db78a186304baf181ddefb89fd408cf3
Franco Campaiola <francocampaiola@hotmail.com>
2022-01-28 20:26:59 -0300

    :+1: Se incorpora un test en consola para revisar un link de API remota

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ import EditarCliente from './paginas/EditarCliente'
 import VerCliente from './paginas/VerCliente'
 
 function App() {
+
+  console.log(import.meta.env.VITE_API_URL);
+
   return (
     <BrowserRouter>
         <Routes>
